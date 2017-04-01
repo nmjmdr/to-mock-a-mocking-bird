a2c6f4aa5a4dfdcfd945013c02553b8cdd142fd5
Mujumdar <mujumdarn@tab101313.local>
2017-04-01 23:36:59 +1100

    replaced with compose function

--- code/ch1.js
+++ code/ch1.js
@@ -22,13 +22,24 @@ P = (x) => {
   return A
 }
 
-
-C = (a,b) => {
+compose = (a,b) => {
   return (x) => a(b(x))
 }
 // Q1
 let x = B
-C(P,B)(x) == P(B(x))
-C(P,M)(x) == P(M(x))
-C(P,x)(x) == P(x(x))
-C(P,C)(C) == P(C(C))
+let C = compose(P,B)
+C(x) == P(B(x))
+C = compose(P,M)
+C(x) == P(M(x))
+C = compose(P,x)
+C(x) == P(x(x))
+C = compose(P,C)
+C(C) == P(C(C))
+
+// Q2
+M(M(x)) == M(x(x))
+M(x(x)) == x(x)(x(x))
+C = compose(M,M)
+C(x) == M(M(x))
+C(x) == x(x)(x(x))
+//C(C) == C(C)(C(C)) //<== leads to Maximum call stack size exceeded
