6f9caa81a59e9b1e8b9e8e0fbf4b0fe2065aa50f
Mujumdar <mujumdarn@tab101313.local>
2017-04-01 22:44:03 +1100

    updated composition function, added soln to q2

--- code/ch1.js
+++ code/ch1.js
@@ -22,12 +22,13 @@ P = (x) => {
   return A
 }
 
-C = (x)=> {
-  return P(B(x))
-}
 
-var x = B
-C(x) == P(B(x))
-C(x) == P(M(x))
-C(x) == P(x(x))
-C(C) == P(C(C))
+C = (a,b) => {
+  return (x) => a(b(x))
+}
+// Q1
+let x = B
+C(P,B)(x) == P(B(x))
+C(P,M)(x) == P(M(x))
+C(P,x)(x) == P(x(x))
+C(P,C)(C) == P(C(C))
